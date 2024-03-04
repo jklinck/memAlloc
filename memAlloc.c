fcf622dcb8dd75b3c9746a2018e7e5e8c74130b5
Joe Klinck <jklinck76@gmail.com>
2024-03-04 12:31:19 -0800

    Update memAlloc.c

--- memAlloc.c
+++ memAlloc.c
@@ -3,28 +3,48 @@
 
 int main(){
 
-    int* x = (int*)malloc(3 * sizeof(int));
-  
+	int* x = (int*)malloc(3 * sizeof(int));
+    
     x[0] = 7;
     x[1] = 9;
     x[2] = 11;
     
     int* temp = realloc(x, 5 * sizeof(int));
-    
-    x = temp;
 
-    /*
-    Since I have x now pointing at the new memory allocated with the temp variable, what is 
-    happening with the original memory created with the x variable on line 6? How would I go 
-    about freeing up that memory so as to not cause a memory leak?
-    */
+    if(temp == NULL){
+        printf("Reallocation was unsuccessful.");
+    }
+    else{
     
-    x[3] = 13;
-    x[4] = 15;
-    
-    printf("%d\n", x[4]);
-    
-    free(x);
+        x = temp;
+        
+        if(&temp[0] != &x[0]){
+            free(x);
+            /*
+            if during the reallocation it does not used the 3 original memory 
+            addresses then I am freeing up the memory that x had allocated, 
+            otherwise we do not want to free it right here because by freeing 
+            x I would also be freeing up temp
+            */
+        }
+        else{
+            /*
+            If temp[0] and x[0] are holding the same memory addresses then proceed
+            */
 
-    return 0;
+            x[3] = 13;
+            x[4] = 15;
+            
+            printf("%d\n", x[4]); // test to check if everything was successful 
+            
+            free(x); 
+            /*
+            this will free up both x and temp since they are pointed to the same 
+            memory address
+            */
+
+        }
+    }
+
+	return 0;
 }
